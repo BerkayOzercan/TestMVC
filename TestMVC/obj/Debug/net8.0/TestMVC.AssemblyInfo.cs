43cbe1490fbfd6be7007eb3d8d5fbfbb6d2aec6e
USER_NAME <berkayster@gmail.com>
2025-01-15 14:06:40 -0600

    add delete option

--- TestMVC/obj/Debug/net8.0/TestMVC.AssemblyInfo.cs
+++ TestMVC/obj/Debug/net8.0/TestMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77763ebf2bff479f337a933b53b1dffa6d9fac94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6edbec05c67656f5a296369ec6027b077c96ddc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
